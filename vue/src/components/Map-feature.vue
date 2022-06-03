cf830d8fca196df22c94d665a5c273ceb19a6161
Tyler Werth <tyler.werth27@gmail.com>
2022-06-03 10:57:48 -0500

    uploading merge of main to my branch

--- vue/src/components/Map-feature.vue
+++ vue/src/components/Map-feature.vue
@@ -1,13 +1,28 @@
 <template>
     <div id="map">
+        <label>
+        
+      </label>
+
         <gmap-map
             :zoom="14"
             :center="center"
             style= "width:100%; height: 600px;"
         >
+        <gmap-marker
+            :key="index"
+            v-for="(m,index) in locationMarkers"
+            :position="m.position"
+            @click="center=m.position"
+        ></gmap-marker>
+
+
+
         </gmap-map>
         <div class="filter">
             <h4>Filter boxes and search stuff</h4>
+            <gmap-autocomplete @place_changed="initMarker"></gmap-autocomplete>
+            <button @click="addLocationMarker">Add</button>
         </div>
     </div>
 </template>
@@ -22,7 +37,10 @@ export default({
             center: {
                 lat: 39.783704,
                 lng: -100.4458825,
-            }
+            },
+            locationMarkers: [],
+            locPlaces: [],
+            existingPlace: null
 
         }
     },
@@ -38,6 +56,21 @@ export default({
                     lng: res.coords.longitude
                 }
             })
+        },
+        initMarker(loc) {
+            this.existingPlace = loc;
+        },
+        addLocationMarker() {
+            if (this.existingPlace) {
+                const marker = {
+                    lat: this.existingPlace.geometry.location.lat(),
+                    lng: this.existingPlace.geometry.location.lng()
+                }
+                this.locationMarkers.push({position: marker})
+                this.locPlaces.push(this.existingPlace)
+                this.center = marker
+                this.existingPlace = null
+            }
         }
     }
 
